5d1b8c23db41a82de19be10460fea69484227a7e
benedict <zzzbenedict3zzz@gmail.com>
2018-12-17 06:59:59 +0200

    Fix all

--- js/animations.js
+++ js/animations.js
@@ -1,8 +1,15 @@
+var percents_block_1 = document.getElementsByClassName('col_1');
+var percents_block_3 = document.getElementsByClassName('col_3');
+
 window.onscroll = function() {
-    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    var scrolled = window.pageYOffset;
     var height = window.innerHeight;
 
     // console.log(slider_mob[0].offsetTop);
+
+    if (scrolled < percents_block_1[0].offsetTop - height*0.8) {
+        $('.count-number').html(0).countTo();
+    }
 
     if (scrolled > 50) {
         $(".second header").css("top", '-50px');
@@ -30,139 +37,30 @@ window.onscroll = function() {
             $(element).removeClass("active");
         }
     });
-
-    if ($(".fives .col_1").hasClass('revealator-within')) {
-
-        (function ($) {
-            $.fn.countTo = function (options) {
-                options = options || {};
-
-                return $(this).each(function () {
-                    // set options for current element
-                    var settings = $.extend({}, $.fn.countTo.defaults, {
-                        from:            $(this).data('from'),
-                        to:              $(this).data('to'),
-                        speed:           $(this).data('speed'),
-                        refreshInterval: $(this).data('refresh-interval'),
-                        decimals:        $(this).data('decimals')
-                    }, options);
-
-                    // how many times to update the value, and how much to increment the value on each update
-                    var loops = Math.ceil(settings.speed / settings.refreshInterval),
-                        increment = (settings.to - settings.from) / loops;
-
-                    // references & variables that will change with each update
-                    var self = this,
-                        $self = $(this),
-                        loopCount = 0,
-                        value = settings.from,
-                        data = $self.data('countTo') || {};
-
-                    $self.data('countTo', data);
-
-                    // if an existing interval can be found, clear it first
-                    if (data.interval) {
-                        clearInterval(data.interval);
-                    }
-                    data.interval = setInterval(updateTimer, settings.refreshInterval);
-
-                    // initialize the element with the starting value
-                    render(value);
-
-                    function updateTimer() {
-                        value += increment;
-                        loopCount++;
-
-                        render(value);
-
-                        if (typeof(settings.onUpdate) == 'function') {
-                            settings.onUpdate.call(self, value);
-                        }
-
-                        if (loopCount >= loops) {
-                            // remove the interval
-                            $self.removeData('countTo');
-                            clearInterval(data.interval);
-                            value = settings.to;
-
-                            if (typeof(settings.onComplete) == 'function') {
-                                settings.onComplete.call(self, value);
-                            }
-                        }
-                    }
-
-                    function render(value) {
-                        var formattedValue = settings.formatter.call(self, value, settings);
-                        $self.html(formattedValue);
-                    }
-                });
-            };
-
-            $.fn.countTo.defaults = {
-                from: 0,               // the number the element should start at
-                to: 0,                 // the number the element should end at
-                speed: 1000,           // how long it should take to count between the target numbers
-                refreshInterval: 100,  // how often the element should be updated
-                decimals: 0,           // the number of decimal places to show
-                formatter: formatter,  // handler for formatting the value before rendering
-                onUpdate: null,        // callback method for every time the element is updated
-                onComplete: null       // callback method for when the element finishes updating
-            };
-
-            function formatter(value, settings) {
-                return value.toFixed(settings.decimals);
-            }
-        }(jQuery));
-
-        jQuery(function ($) {
-            // custom formatting example
-            $('.count-number').data('countToOptions', {
-                formatter: function (value, options) {
-                    return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
-                }
-            });
-
-            // start all the timers
-            $('.timer').each(count);
-
-            function count(options) {
-                var $this = $(this);
-                options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-                $this.countTo(options);
-            }
-        });
-
-        jQuery(function ($) {
-            // custom formatting example
-            $(".revealator-within .count-number").data('countToOptions', {
-                formatter: function (value, options) {
-                    return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
-                }
-            });
-
-            // start all the timers
-            $('.timer').each(count);
-
-            function count(options) {
-                var $this = $(this);
-                options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-                $this.countTo(options);
-            }
-        });
-
-    }
-
 };
 
 $(document).ready(function () {
+
+    var video = document.getElementById('video_file');
+    var play_video = function () {
+        $('.video_clip').show();
+        video.play();
+    };
+    var pause_video = function () {
+        video.pause();
+        $('.video_clip').hide();
+    };
+
+    $('.play_block').click(play_video);
+
+    $('.right_col').click(play_video);
+    $('#video_file').click(pause_video);
+
+
     if (window.innerWidth > 1024) {
         $(".slider li").each(function (index, element) {
             $(element).css("opacity", "1");
         })
-    }
-    var graphic_2 = $('.graphs');
-    if (graphic_2) {
-        console.log('test');
     }
 
 });
